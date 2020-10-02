93be6a7de30abe660f5954318d871a910fcdf444
lukas <lukasz.kodym@gmail.com>
2020-10-02 07:43:00 +0200

    update

--- built-in_modules/date.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-d1 = datetime.date(2021, 1, 1)
-d2 = datetime.date(2021, 7, 31)
-d3 = datetime.date(1990, 5, 7)
-
-print(d1)
-print(d2)
-print(d3)
--- built-in_modules/datetime/ex_1.py
+++ built-in_modules/datetime/ex_1.py
@@ -1,5 +1,9 @@
 import datetime
 
+d1 = datetime.date(2021, 1, 1)
+d2 = datetime.date(2021, 7, 31)
+d3 = datetime.date(1990, 5, 7)
 
-class date(y, m, d):
-    datetime.date(y, m, d)
+print(d1)
+print(d2)
+print(d3)
