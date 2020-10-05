96fac0e01a6438abf1861d9fa2acffba7e0b4fa3
lukas <lukasz.kodym@gmail.com>
2020-10-05 20:41:32 +0200

    update

--- built-in_modules/datetime/ex_10.py
+++ built-in_modules/datetime/ex_10.py
@@ -7,7 +7,13 @@ dt_30d = dt + timedelta(days=30)
 dt_30h = dt + timedelta(hours=30)
 dt_15m = dt + timedelta(minutes=15)
 
+# alternative solution
+"""import datetime
+dt = datetime(2020, 1, 1)
+dt_7d = dt + datetime.timedelta(days=7)"""
+
 print(dt_7d)
 print(dt_30d)
 print(dt_30h)
 print(dt_15m)
+
