f06b61bf9ab6ac7961c749a263b5ca85407e25f5
LukasKodym <lukasz.kodym@gmail.com>
2020-10-10 17:48:31 +0200

    os module in progress

--- built-in_modules/os/ex_11.py
+++ built-in_modules/os/ex_11.py
@@ -4,10 +4,12 @@ fnames = [f'{str(i).zfill(2)}_sales.csv' for i in range(1, 25)]
 
 y_20 = '2020'
 y_21 = '2021'
+i = 0
 
 for name in sorted(fnames):
-    if len(fnames) < 13:
-        d_20 = os.path.join(os.getcwd(), y_20, name)
-    else:
-        d_21 = os.path.join(os.getcwd(), y_21, name)
 
+    if i < 12:
+        print(os.path.join(os.getcwd(), y_20, name))
+    else:
+        print(os.path.join(os.getcwd(), y_21, name))
+    i += 1
