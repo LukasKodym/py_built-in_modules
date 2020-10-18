3157785bc4ac25ab67905807ba9d890b86e85d23
LukasKodym <lukasz.kodym@gmail.com>
2020-10-18 11:49:01 +0200

    copy module in progress

---
+++ built-in_modules/copy/ex_02.py
@@ -0,0 +1,9 @@
+import copy
+
+stocks = [['CDR', '11B'], ['PLW'], ['TEN']]
+
+stocks_copied = copy.deepcopy(stocks)
+
+stocks[0][1] = 'CRJ'
+
+print(f'stocks: {stocks}\nstocks_copied: {stocks_copied}')
