c1f26b87b07aa48f23bd7bc4aa701f0ea59570c6
LukasKodym <lukasz.kodym@gmail.com>
2020-10-25 15:04:10 +0100

    collections in progress

---
+++ built-in_modules/collections/ex_08.py
@@ -0,0 +1,10 @@
+##
+
+from collections import ChainMap
+
+stocks_1 = {'CDR': 400, 'PLW': 490}
+stocks_2 = {'PLW': 500, 'TEN': 550, 'BBT': 30}
+
+sum_stock = ChainMap(stocks_1, stocks_2)
+
+print(sum_stock)
