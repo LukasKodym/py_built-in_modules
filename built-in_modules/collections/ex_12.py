4814613e496005d15cf91a0f63080d8da0f7fd5a
LukasKodym <lukasz.kodym@gmail.com>
2020-10-27 18:36:59 +0100

    collections in progress

---
+++ built-in_modules/collections/ex_12.py
@@ -0,0 +1,13 @@
+##
+
+from collections import ChainMap
+
+
+techs = {'Apple': 370, 'Samsung': 1100, 'Microsoft': 201}
+finance = {'Citigroup': 51, 'Allianz': 180}
+gaming = {'Sony': 76, 'Nintendo': 470, 'EA': 135}
+
+stocks = ChainMap(techs, finance, gaming)
+
+print(sorted([k for k, v in stocks.items()]))
+# print(sorted(list(stocks.keys())))
