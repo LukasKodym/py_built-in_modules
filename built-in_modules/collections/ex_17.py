eadab034b06da1c63c25f07f3fcabc7b4e2e5263
LukasKodym <lukasz.kodym@gmail.com>
2020-11-01 13:00:44 +0100

    collections in progress

---
+++ built-in_modules/collections/ex_17.py
@@ -0,0 +1,14 @@
+##
+
+from collections import namedtuple
+
+Student = namedtuple(typename='Student', field_names=['name', 'age', 'specialization'])
+
+st1 = Student('Mike', 21, 'physic')
+st2 = Student('Kate', 20, 'mathematics')
+st3 = Student('Bob', 21, 'information technology')
+
+students = [st1, st2, st3]
+
+for student in students:
+    print(f'{student.name:5}: {student.age}: {student.specialization}')
