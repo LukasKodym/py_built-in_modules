db19374dc7d9186bff4338cdd3c8020dad89a3f9
LukasKodym <lukasz.kodym@gmail.com>
2020-11-01 13:03:03 +0100

    collections in progress

--- built-in_modules/collections/ex_17.py
+++ built-in_modules/collections/ex_17.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 
 Student = namedtuple(typename='Student', field_names=['name', 'age', 'specialization'])
 
-st1 = Student('Mike', 21, 'physic')
+st1 = Student('Mike', 21, 'physics')
 st2 = Student('Kate', 20, 'mathematics')
 st3 = Student('Bob', 21, 'information technology')
 
